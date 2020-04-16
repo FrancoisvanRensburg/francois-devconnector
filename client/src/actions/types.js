384217b90fdfa4d0f8f7c6cb661cfb05744a423e
Francois van Rensburg <jvrensburgfrans@gmail.com>
2020-04-16 09:27:09 +0200

    Alert type and action set up. Reducer pointing to registration alert initialised and default timeout implimented

---
+++ client/src/actions/types.js
@@ -0,0 +1,7 @@
+// Only frontend
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+// http request (backend) - if registration is a success dispatch REGISTER_SUCCESSFULL. If it's a fail, dispatch REGISTER_FAIL
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,6 @@
+import {REGISTER_SUCCESS, REGISTER_FAIL} from '../actions/types'
+
+const initialState = {
+    // Token that we get back from backent (jwt from api)
+    token: 
+}
